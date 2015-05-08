0f3cd2851f1e138c74aac298c5bf375441a62df7
DanielJunior <danieljunior@id.uff.br>
2015-05-08 10:57:37 -0300

    Iniciando o split da frase

--- processaArquivo.php
+++ processaArquivo.php
@@ -2,8 +2,21 @@
 
 <?php
 $arq = $_FILES['data']['tmp_name'];
-echo file_get_contents($arq);
-$temp = "json_format.json";
+
+$ponteiro = fopen($arq, "r");
+
+while (!feof($ponteiro)) {
+
+    $linha = fgets($ponteiro);
+    $array = explode("=", $linha);
+    echo $linha . "<br>";
+    echo "AGORA O ARRAY<br>";
+    echo $array[0]. "<br>";
+}
+fclose($ponteiro);
+
+//echo file_get_contents($arq);
+//$temp = "json_format.json";
 //rename($arq, $temp);
 //header("Content-type: text/html; charset=utf-8");
 //$json = file_get_contents($temp, 0, null, null);
